fe4b40fbe09113311af4a5cb84ff076784fbd4f8
skymoneyc <CChain0615@gmail.com>
2015-03-19 17:07:17 +0800

    add oauth funcs

--- play-oauthlib/app/controllers/Authorize.java
+++ play-oauthlib/app/controllers/Authorize.java
@@ -1,10 +1,17 @@
 package controllers;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import models.Client;
+import models.Grant;
+import models.Token;
 import models.User;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import util.AuthorizeUtil;
@@ -29,7 +36,8 @@ public class Authorize extends Controller{
 				session("user", String.valueOf(user.id));
 			}
 			
-			return ok(index.render("Home page ", user));
+			//return ok(index.render("Home page ", user));
+			return redirect("/authorize?response_type=code&client_id=abc&redirect=http://localhost:8000&scope=email");
 		}
 		
 		User user = null;
@@ -54,27 +62,50 @@ public class Authorize extends Controller{
 		
 		if(request().method() == "POST"){
 			//确认认证, 则跳转到应用的回调uri并附上code，以便应用凭code换取token
+			
+			Map<String, String[]> confirm_data = request().body().asFormUrlEncoded();
+			
+			String confirm = confirm_data.get("yes")[0];
+			
+			if(confirm.equals("yes")){
+				String responseType = confirm_data.get("response_type")[0];
+				String clientId = confirm_data.get("client_id")[0];
+				String redirectUri = confirm_data.get("redirect_uri")[0];
+				String scope = confirm_data.get("scope")[0];
+				int userId = Integer.parseInt(session().get("user"));
+				
+				//生成一个grant并存入数据库，并返回code
+				String code = AuthorizeUtil.generate_grant(responseType, clientId, redirectUri, scope, userId);
+				
+				//跳转到client的回调uri
+				return redirect(redirectUri + "?code=" + code);
+			}else{
+				//不同意授权，跳转
+				//return redirect("");
+				return ok("not allowed");
+			}
 		}
 		
 		int user_id = Integer.parseInt(session().get("user"));
+		
+		User user = User.findById(user_id);
+		
 		//根据oauth2 协议，必须参数包括 
 		//response_type
 		//client_id
 		//redirect_uri
 		//scope
-		String response_type = request().getQueryString("response_type");
-		String client_id = request().getQueryString("client_id");
-		String redirect_uri = request().getQueryString("redirect_uri");
+		String responseType = request().getQueryString("response_type");
+		String clientId = request().getQueryString("client_id");
+		String redirectUri = request().getQueryString("redirect_uri");
 		String scope = request().getQueryString("scope");
 		
-		if(!AuthorizeUtil.check_redirect_uri(client_id, redirect_uri)){
+		if(!AuthorizeUtil.check_redirect_uri(clientId, redirectUri)){
 			//校验回调uri，不对则跳转
-			return redirect("");
+			return ok("redirect uri invalid");
 		}
 		
-		//生成一个grant并存入数据库
-		AuthorizeUtil.generate_grant(response_type, client_id, redirect_uri, scope, user_id);
-		return ok();
+		return ok(authorize.render(user, clientId, responseType, redirectUri, scope));
 	}
 	
 	/**
@@ -84,25 +115,58 @@ public class Authorize extends Controller{
 	 * 直接返回token 包括 access_token 和 refresh_token
 	 * */
 	public static Result token(){
-		String code = request().getQueryString("code");
-		
-		//根据code去grant数据中查找，如果存在且未超时，则获取client_id 等信息，
-		//生成一个token，并删除grant
-		
 		//根据oauth2协议，必须参数
 		//grant_type
 		//client_id
 		//client_srcret
 		//code
 		//redirect_uri
-		String grant_type = request().getQueryString("grant_type");
-		String client_id = request().getQueryString("client_id");
-		String client_secret = request().getQueryString("client_secret");
+		String grantType = request().getQueryString("grant_type");
+		String clientId = request().getQueryString("client_id");
+		String clientSecret = request().getQueryString("client_secret");
+		String code = request().getQueryString("code");
+				
+		//根据code去grant数据中查找，如果存在且未超时，则获取client_id 等信息，
+		//生成一个token，并删除grant
+		Grant grant = Grant.findByCode(code);
 		
+		Date now = new Date();
+		
+		//检查是否符合client
+		if(!AuthorizeUtil.checkClientValid(code, clientId, clientSecret)){
+			return ok("not valid client");
+		}
+		
+		//检查是否已经超过expires		
+		if(now.before(grant.expires)){
+			//超过过期时间
+			return ok("expires， re-ensure aggin");
+		}
+		
+		//生成一个token
+		Token token = new Token();
+		token.cliendId = clientId;
+		token.userId = Integer.parseInt(session().get("userId"));
+		token.scope = grant.scope;
+		token.tokenType = "Bearer";
+		
+		Date expires = new Date();
+		expires.setTime(expires.getTime()/1000 + 60 * 60 * 24 * 30);
+		
+		token.expires = expires;
+		
+		String access_token = "";
+		String refresh_token = "";
+		
+		//保存token
+		token.save();		
 		
 		response().setContentType("appliction/json");
-		JsonNode jn;
-		return ok("");
+		ObjectNode token_return = Json.newObject();
+		token_return.put("access_token", access_token);
+		token_return.put("refresh_token", refresh_token);
+		
+		return ok(token_return);
 	}
 	
 	/**
--- play-oauthlib/app/util/AuthorizeUtil.java
+++ play-oauthlib/app/util/AuthorizeUtil.java
@@ -1,11 +1,32 @@
 package util;
 
+import java.util.Date;
+import java.util.Random;
+
+import models.Client;
+import models.Grant;
+
 public class AuthorizeUtil {
 	/**
 	 * 校验回调uri，防止串改
 	 * */
-	public static boolean check_redirect_uri(String client_id, String redirect_uri){
-		return true;
+	public static boolean check_redirect_uri(String clientId, String redirectUri){
+		Client client = Client.findById(clientId);
+		if(client.redirect_uri.equals(redirectUri)){
+			return true;
+		}
+		return false;
+	}
+	
+	public static boolean checkClientValid(String code, String clientId, String clientSecret){
+		Grant grant = Grant.findByCode(code);
+		String grant_clientId = grant.clientId;
+		Client client = Client.findById(grant_clientId);
+		
+		if(client.clientId.equals(clientId) && client.clientSecret.equals(clientSecret)){
+			return true;
+		}
+		return false;
 	}
 	
 	/**
@@ -13,8 +34,45 @@ public class AuthorizeUtil {
 	 * 
 	 * 返回grant id
 	 * */
-	public static void generate_grant(String response_tyep, String client_id, String redirect_uri, 
-			String scope, int user_id){
+	public static String generate_grant(String responseType, String clientId, String redirectUri, 
+			String scope, int userId){
+		Grant grant = new Grant();
 		
+		grant.responseType = responseType;
+		grant.clientId = clientId;
+		grant.redirectUri = redirectUri;
+		grant.scope = scope;
+		grant.userId = userId;
+		
+		//随机生成code
+		String code = generateRandomString(40);
+		
+		grant.code = code;
+		
+		Date current = new Date();
+		long time = current.getTime()/1000 + 60 * 5;
+		current.setTime(time*1000);
+		
+		grant.expires = current;
+		
+		grant.save();
+		
+		return code;
+	}
+	
+	/**
+	 * 根据传入参数生成对应长度的随机字符串
+	 * */
+	public static String generateRandomString(int length){
+		StringBuffer allChar = new StringBuffer("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		StringBuffer sb = new StringBuffer();
+		Random r = new Random();
+		int range = allChar.length();
+		
+		for(int i=0;i<length;i++){
+			sb.append(allChar.charAt(r.nextInt(range)));
+		}
+		
+		return sb.toString();
 	}
 }
