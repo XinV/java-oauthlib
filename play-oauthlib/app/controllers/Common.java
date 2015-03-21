643887059b8ccdd03ab89d6b94cbcb070b518f22
skymoneyc <CChain0615@gmail.com>
2015-03-21 15:10:40 +0800

    add client create router

--- play-oauthlib/app/controllers/Common.java
+++ play-oauthlib/app/controllers/Common.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import java.util.Map;
+
+import models.Client;
 import play.mvc.Controller;
 import play.mvc.Result;
+import util.AuthorizeUtil;
+import views.html.client;
 
 public class Common extends Controller{
 	
@@ -9,6 +14,31 @@ public class Common extends Controller{
 	 * 生成一个client
 	 * */
 	public static Result client(){
-		return ok();
+		if(request().method() == "POST"){
+			//新建一个client
+			Map<String, String[]> clientInfoForm = request().body().asFormUrlEncoded();
+			
+			String clientName = clientInfoForm.get("client_name")[0];
+			String clientUri = clientInfoForm.get("client_uri")[0];
+			
+			String clientId = AuthorizeUtil.generateRandomString(20);
+			String clientSecret = AuthorizeUtil.generateRandomString(30);
+			
+			Client client = new Client();
+			client.clientName = clientName;
+			client.clientId = clientId;
+			client.clientSecret = clientSecret;
+			client.redirect_uri = clientUri;
+			
+			client.scope = "email";
+			
+			client.save();
+			
+			//创建client成功
+			
+			return ok("client create done...");
+		}
+		
+		return ok(client.render());
 	}
 }
